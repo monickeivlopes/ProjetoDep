c65e7c0724ca8cc3821a0e9e7a022c335b3dc274
Monicke Lopes <99441637+monickeivlopes@users.noreply.github.com>
2025-07-10 15:59:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,91 @@ def register():
 
         User.create(nome=nome, email=email, telefone=telefone, senha=senha,tipo=tipo,cpf=cpf)
 
+
         
         flash("Cadastro realizado com sucesso! Faça login.", "success")
         return redirect(url_for('login'))
     return render_template('registro.html')
+
+
+#página de hospedes
+@app.route('/hospedes', methods=['GET'])
+@login_required
+def hospedes():
+    nome_filtro = request.args.get('nome', '')  
+    ordem = request.args.get('ordenar', 'asc')  
+
+    if ordem == 'asc':
+        order_by = 'ASC'
+    else:
+        order_by = 'DESC'
+
+    cur = mysql.connection.cursor()
+
+    if nome_filtro:
+        cur.execute("SELECT * FROM hospede WHERE nome LIKE %s and tipo = 'usuario' ORDER BY nome  " + order_by, (nome_filtro + '%',))
+    else:
+        cur.execute("SELECT * FROM hospede WHERE tipo = 'usuario' ORDER BY nome  " + order_by)
+
+    hospedes = cur.fetchall()  
+    cur.close()
+
+    user = current_user
+
+
+    if not user:
+        flash("Usuário não encontrado.", "error")
+        return redirect(url_for("login"))
+
+    if user.tipo == 'usuario':
+        flash('Você não permissão para entrar')
+        return render_template('quartos.html')
+    else:
+        barra = True
+        return render_template('hospedes.html', hospedes=hospedes, barra=barra)
+
+#página para adicionar hóspedes
+@app.route('/add_hospede', methods=['GET', 'POST'])
+@login_required
+def add_hospede():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        email = request.form['email']
+        tipo = request.form['tipo']
+        senha = request.form['senha']
+
+       
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM hospede WHERE cpf = %s",(cpf,))
+        existing_cpf = cur.fetchone()
+
+        cur.execute("SELECT * FROM hospede WHERE email = %s", (email,))
+        existing_email = cur.fetchone()
+        
+        if existing_cpf or existing_email:
+            texto=('Já existe um hóspede com este CPF ou e-mail. Tente novamente com dados diferentes.')
+            return render_template('add_hospedes.html', flash=texto)
+
+        
+        cur.execute("INSERT INTO hospede (nome, cpf, telefone, email,senha, tipo) VALUES (%s, %s, %s, %s, %s, %s)",
+                    (nome, cpf, telefone, email, senha, tipo))
+        mysql.connection.commit()
+        cur.close()
+
+        
+        return redirect(url_for('hospedes'))  
+
+    user = current_user
+
+
+    if not user:
+        flash("Usuário não encontrado.", "error")
+        return redirect(url_for("login"))
+
+    if user.tipo == 'usuario':
+        flash('Você não permissão para adicionar', 'error')
+        return render_template('quartos.html')
+    else:
+        return render_template('add_hospedes.html')   
